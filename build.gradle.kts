a0a487e1a59a21376c53d736767bacef7b44b7d6
Alexander Popoff <alexander.popoff@flysto.net>
2025-01-08 12:53:27 +0200

    Kotlin Wrappers `2025.1.1` & Gradle Wrapper `8.12`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
-import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
 
 plugins {
     kotlin("multiplatform")
@@ -26,10 +24,10 @@ kotlin {
                 implementation(kotlinWrappers.emotion)
                 implementation(kotlinWrappers.mui.material)
                 implementation(kotlinWrappers.mui.iconsMaterial)
-//                implementation(kotlinWrappers.muix.datePickers)
-//
-//                implementation(npm("date-fns", "2.30.0"))
-//                implementation(npm("@date-io/date-fns", "2.17.0"))
+                implementation(kotlinWrappers.muix.datePickers)
+
+                implementation(npm("date-fns", "2.30.0"))
+                implementation(npm("@date-io/date-fns", "2.17.0"))
             }
         }
     }
@@ -41,13 +39,6 @@ tasks.withType<KotlinJsCompile>().configureEach {
     }
 }
 
-plugins.withType<NodeJsRootPlugin> {
-    the<NpmExtension>().apply {
-        override("react", "^19.0.0")
-        override("react-dom", "^19.0.0")
-    }
-}
-
 tasks.wrapper {
-    gradleVersion = "8.10"
+    gradleVersion = "8.12"
 }
--- src/jsMain/kotlin/io/github/aerialist7/showcase/material/muix/DateAndTimePickers.kt
+++ src/jsMain/kotlin/io/github/aerialist7/showcase/material/muix/DateAndTimePickers.kt
@@ -1,13 +1,15 @@
 package io.github.aerialist7.showcase.material.muix
 
+import muix.pickers.AdapterDateFns
+import muix.pickers.DatePicker
+import muix.pickers.LocalizationProvider
 import react.FC
 import react.Props
 
 val DateAndTimePickersShowcase = FC<Props> {
-//    LocalizationProvider {
-//        dateAdapter = AdapterDateFns
-//        CalendarPicker {
-//            onChange = { _, _ -> }
-//        }
-//    }
+    LocalizationProvider {
+        dateAdapter = AdapterDateFns
+
+        DatePicker()
+    }
 }
